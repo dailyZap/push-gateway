e54718e116ae8cd2c5fafb13a49602402ebfd86b
Hannes Rüger <hannes@rueger.events>
2024-09-14 11:22:19 +0200

    feat: timezones for moments

--- src/routes/api/v1/moments/+server.ts
+++ src/routes/api/v1/moments/+server.ts
@@ -28,12 +28,12 @@ export const GET: RequestHandler = async ({ request, url }) => {
 			},
 			where: afterTimestamp
 				? {
-						timestamp: {
+						date: {
 							gt: afterTimestamp
 						}
 					}
 				: {
-						timestamp: {
+						date: {
 							gte: yesterdayStart
 						}
 					},
@@ -41,7 +41,13 @@ export const GET: RequestHandler = async ({ request, url }) => {
 		})
 	).map((m) => ({
 		id: m.id,
-		timestamp: m.timestamp.getTime()
+		date: m.date.getTime(),
+		time: {
+			EU: m.timestampEU.getTime(),
+			US: m.timestampUS.getTime(),
+			WA: m.timestampWA.getTime(),
+			EA: m.timestampEA.getTime()
+		}
 	}));
 	return ok({
 		moments: moments
@@ -53,44 +59,74 @@ async function ensureMomentsForMonth() {
 	const yesterday = new Date();
 	yesterday.setDate(yesterday.getDate() - 1);
 	const todayStart = new Date(yesterday.getFullYear(), yesterday.getMonth(), yesterday.getDate());
-	const todayEnd = new Date(yesterday.getFullYear(), yesterday.getMonth(), yesterday.getDate() + 1);
 	let todayMoment = await prisma.moment.findFirst({
 		where: {
-			AND: [{ timestamp: { gte: todayStart } }, { timestamp: { lt: todayEnd } }]
+			date: todayStart
 		}
 	});
 	if (!todayMoment) {
 		todayMoment = await createMoment(todayStart);
 	}
-	for (let i = 1; i < 30; i++) {
-		const date = new Date(yesterday.getFullYear(), yesterday.getMonth(), yesterday.getDate() + i);
-		const start = new Date(date.getFullYear(), date.getMonth(), date.getDate());
-		const end = new Date(date.getFullYear(), date.getMonth(), date.getDate() + 1);
-		let moment = await prisma.moment.findFirst({
-			where: {
-				AND: [{ timestamp: { gte: start } }, { timestamp: { lt: end } }]
-			}
-		});
-		if (!moment) {
-			moment = await createMoment(start);
+
+	// most future moment
+	const futureMoment = await prisma.moment.findFirstOrThrow({
+		orderBy: {
+			date: 'desc'
+		}
+	});
+	const mostDaysInAdvance = 30;
+	// if not enough moments, create more
+	if (
+		!futureMoment ||
+		(futureMoment.date.getTime() - todayStart.getTime()) / (1000 * 60 * 60 * 24) < mostDaysInAdvance
+	) {
+		const daysToCreate =
+			mostDaysInAdvance -
+			(futureMoment.date.getTime() - todayStart.getTime()) / (1000 * 60 * 60 * 24);
+		for (let i = 0; i < daysToCreate; i++) {
+			await createMoment(
+				new Date(todayStart.getFullYear(), todayStart.getMonth(), todayStart.getDate() + i)
+			);
 		}
 	}
 }
 
 async function createMoment(date: Date) {
-	const timestamp = new Date(
-		date.getFullYear(),
-		date.getMonth(),
-		date.getDate(),
-		9 + Math.floor(Math.random() * 12),
-		Math.floor(Math.random() * 60),
-		Math.floor(Math.random() * 60),
-		Math.floor(Math.random() * 1000)
-	);
+	const randomTimeStamp = () =>
+		new Date(
+			date.getFullYear(),
+			date.getMonth(),
+			date.getDate(),
+			9 + Math.floor(Math.random() * 12),
+			Math.floor(Math.random() * 60),
+			Math.floor(Math.random() * 60),
+			Math.floor(Math.random() * 1000)
+		);
+
+	const timezoneOffsetsHours = {
+		EU: 0,
+		US: -6,
+		WA: -8,
+		EA: 8
+	};
+
+	const timestampEU = randomTimeStamp();
+	const timestampUS = new Date(timestampEU);
+	timestampUS.setHours(timestampEU.getHours() + timezoneOffsetsHours.US);
+	const timestampWA = new Date(timestampEU);
+	timestampWA.setHours(timestampEU.getHours() + timezoneOffsetsHours.WA);
+	const timestampEA = new Date(timestampEU);
+	timestampEA.setHours(timestampEU.getHours() + timezoneOffsetsHours.EA);
+
+	const justDate = new Date(date.getFullYear(), date.getMonth(), date.getDate());
 	return await prisma.moment.create({
 		data: {
-			timestamp,
-			id: id('Moment')
+			id: id('Moment'),
+			date: justDate,
+			timestampEU,
+			timestampUS,
+			timestampWA,
+			timestampEA
 		}
 	});
 }

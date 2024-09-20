e09a271161458f91c3af3737850ad3788c66f7d1
Hannes Rüger <hannes@rueger.events>
2024-09-20 09:33:40 +0200

    feat: redesign landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,25 @@
-Landing Page
-
-<a href="/auth/login">Login</a>
-<a href="/auth/register">Register</a>
+<div class="min-h-screen flex flex-col items-center justify-center">
+	<img src="/assets/icon_dark.png" alt="dailyZap Logo" class="w-32 mx-auto mb-10" />
+	<div class="text-center">
+		<h1 class="text-4xl font-bold mb-4">dailyZap Notification Gateway</h1>
+		<p class="text-lg mb-6">
+			When hosting your own dailyZap server, register here to send push notifications to your users.
+		</p>
+		<div class="space-y-4 mt-10">
+			<a href="https://dailyzap.me" class="btn btn-primary btn-wide">
+				Learn More at dailyZap.me
+				<i class="fas fa-external-link-alt"></i>
+			</a>
+			<div class="flex space-x-4 justify-center">
+				<a href="/auth/register" class="btn btn-outline">
+					Register
+					<i class="fas fa-user-plus"></i>
+				</a>
+				<a href="/auth/login" class="btn btn-secondary">
+					Login
+					<i class="fas fa-sign-in"></i>
+				</a>
+			</div>
+		</div>
+	</div>
+</div>
